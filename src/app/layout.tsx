e1076f0780f43e15a33adfe9e1f6868cc1b81232
Archit Kakkar <education.archit@gmail.com>
2025-12-22 01:22:05 +0530

    setup shadcn.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Alan_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const alanSans = Alan_Sans({
+  variable: "--font-sans",
   subsets: ["latin"],
 });
 
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${alanSans.variable} antialiased dark`}>
         {children}
       </body>
     </html>
