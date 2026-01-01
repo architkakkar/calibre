0fed286b450cc8d4b4c3d03c7e04609e03c1d92f
Archit Kakkar <education.archit@gmail.com>
2026-01-02 00:58:26 +0530

    feat(routes): create base file for each navlink routes.

---
+++ src/app/(app)/(shell)/ai-trainer/[...chatId]/page.tsx
@@ -0,0 +1,3 @@
+export default function page() {
+  return <div>Specific chat from history</div>;
+}
---
+++ src/app/(app)/(shell)/ai-trainer/new-chat/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AINewChat() {
+  return <div>AI New Chat</div>;
+}
