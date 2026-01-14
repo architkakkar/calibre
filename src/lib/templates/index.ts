b700a65abf712e25106937612b0e8b915bb9083c
Archit Kakkar <education.archit@gmail.com>
2026-01-15 01:21:30 +0530

    feat(template): add ACTIVE_WORKOUT_PLAN and ACTIVE_NUTRITION_PLAN exports

---
+++ src/lib/templates/index.ts
@@ -0,0 +1,7 @@
+import { WORKOUT_PLAN_V1 } from "@/lib/templates/workout/workout-plan.v1.definition";
+import { NUTRITION_PLAN_V1 } from "@/lib/templates/nutrition/nutrition-plan.v1.definition";
+
+export {
+  WORKOUT_PLAN_V1 as ACTIVE_WORKOUT_PLAN,
+  NUTRITION_PLAN_V1 as ACTIVE_NUTRITION_PLAN,
+};
